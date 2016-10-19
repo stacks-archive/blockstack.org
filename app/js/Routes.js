3850adb557d3bdd20c910c67ab3be2b7a9f1a1f9
Ryan Shea <ryan@shea.io>
2016-10-19 17:01:01 -0400

    update routes

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -8,10 +8,7 @@ import App                         from './App'
 import HomePage                    from './pages/HomePage'
 import SummitPage                  from './pages/SummitPage'
 
-import TutorialsIndexPage          from './pages/TutorialsIndexPage'
-import ArticlesIndexPage           from './pages/ArticlesIndexPage'
-import PapersIndexPage             from './pages/PapersIndexPage'
-
+import PapersPage                  from './pages/PapersPage'
 import ArticlePage                 from './pages/ArticlePage'
 import NotFoundPage                from './pages/NotFoundPage'
 import AboutPage                   from './pages/AboutPage'
@@ -26,17 +23,15 @@ export default (
 
       <Route path="/" component={HomePage} />
 
+      <Route path="/about" component={AboutPage} />
       <Route path="/docs" component={DocsPage} />
-      <Route path="/articles" component={ArticlesIndexPage} />
-      <Route path="/tutorials" component={TutorialsIndexPage} />
-      <Route path="/papers" component={PapersIndexPage} />
+      <Route path="/papers" component={PapersPage} />
+      <Route path="/summit" component={SummitPage} />
+
       <Route path="/articles/:docSection" component={ArticlePage} />
       <Route path="/papers/:docSection" component={ArticlePage} />
       <Route path="/tutorials/:docSection" component={ArticlePage} />
       <Route path="/docs/:docSection" component={ArticlePage} />
-
-      <Route path="/about" component={AboutPage} />
-      <Route path="/summit" component={SummitPage} />
 
       <Route path="*" component={NotFoundPage} />
 
