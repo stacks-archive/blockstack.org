78ce77ab260c8aff8786d104c97797e128e3ca10
Guy Lepage <guylepage3@gmail.com>
2017-07-23 11:01:26 -0400

    token hero restructuring

--- app/js/pages/TokenSalePage.js
+++ app/js/pages/TokenSalePage.js
@@ -1,10 +1,11 @@
 'use strict'
 
-import {Component}      from 'react'
-import {Link}           from 'react-router'
-import DocumentTitle    from 'react-document-title'
+import {Component}         from 'react'
+import {Link}              from 'react-router'
+import DocumentTitle       from 'react-document-title'
 
-import MailchimpForm from '../components/MailchimpForm'
+import MailchimpForm       from '../components/MailchimpForm'
+import TransparentHeader   from '../components/TransparentHeader'
 
 class TokenSalePage extends Component {
 
@@ -17,32 +18,23 @@ class TokenSalePage extends Component {
 
     return (
       <DocumentTitle title="Blockstack - Token Sale">
-        <div>
-          <div className="container-fluid col-centered summit-head-wrap">
-            <div className="p-t-3 p-b-3" style={{
-              color: 'white',
-              position: 'relative',
-              marginTop: '0px !important',
-              height: '500px'
-            }}>
-              <section>
-                <div>
-                  <div className="col-xs-11 col-sm-10 col-centered">
-                    <h1 className="hero-head summ-head">The Blockstack Token Sale</h1>
-                    <p className="lead hero-comm-des">A new internet for decentralized apps</p>
-                    <div className="row">
-                      <div className="col-md-6 offset-md-3 m-t-2 m-b-3">
-                        <MailchimpForm subscribeURL={subscribeURL} submitButtonText='Sign Up' />
-                      </div>
-                    </div>
-                    <p className="text-xs-center m-t-2 m-b-5">
-                      <Link to="/whitepaper.pdf" target="_blank" className="btn btn-special btn-lg">
-                        Read the Whitepaper
-                      </Link>
-                    </p>
+        <div className="body-hero">
+          <div className="col-centered block">
+            <TransparentHeader />
+            <div className="container">
+              <section className="hero">
+                <h1 className="hero-head summ-head">The Blockstack Token Sale</h1>
+                <p className="lead hero-comm-des">A new internet for decentralized apps</p>
+                <div className="row">
+                  <div className="col-md-6 offset-md-3 m-t-2 m-b-3">
+                    <MailchimpForm subscribeURL={subscribeURL} submitButtonText='Sign Up' />
                   </div>
-                  <div className="img"></div>
                 </div>
+                <p className="text-xs-center m-t-2 m-b-5">
+                  <Link to="/whitepaper.pdf" target="_blank" className="btn btn-special btn-lg">
+                    Read the Whitepaper
+                  </Link>
+                </p>
               </section>
             </div>
           </div>
