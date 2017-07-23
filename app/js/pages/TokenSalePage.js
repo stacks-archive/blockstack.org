b931baab4352456bcae2331bed17429a43dc2b1e
Guy Lepage <guylepage3@gmail.com>
2017-07-23 16:09:48 -0400

    finalized hero spacing and whitepaper button

--- app/js/pages/TokenSalePage.js
+++ app/js/pages/TokenSalePage.js
@@ -27,15 +27,15 @@ class TokenSalePage extends Component {
                 <Image className="stacks-logo-sm m-t-90 m-b-55"
                         src="/images/logos/stacks-logo.svg"
                         retinaSupport={false} />
-                <h1 className="text-white">The Blockstack Token Sale</h1>
+                <h1 className="text-white m-b-20">The Blockstack Token Sale</h1>
                 <p className="hero-lead purple-50">Sign up to receive important information about the token sale</p>
                 <div className="row">
                   <div className="col-md-9 col-centered m-t-2 m-b-3">
                     <DarkForm subscribeURL={subscribeURL} submitButtonText='Sign Up' />
                   </div>
                 </div>
-                <p className="text-xs-center m-t-2 m-b-5">
-                  <Link to="/whitepaper.pdf" target="_blank" className="btn btn-special btn-lg">
+                <p className="text-xs-center m-t-2 m-b-100">
+                  <Link to="/whitepaper.pdf" target="_blank" className="btn btn-outline-primary">
                     Read the Whitepaper
                   </Link>
                 </p>
