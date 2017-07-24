25ad86e57d87f50e5d30e0b936bb4e79857a2d6e
Ryan Shea <ryan@shea.io>
2017-07-24 09:46:52 -0400

    add video section to token sale page

--- app/js/pages/TokenSalePage.js
+++ app/js/pages/TokenSalePage.js
@@ -7,16 +7,20 @@ import DocumentTitle       from 'react-document-title'
 import DarkForm            from '../components/DarkForm'
 import TransparentHeader   from '../components/TransparentHeader'
 import Image               from '../components/Image'
+import EmbedYouTube        from '../components/EmbedYouTube'
 
 class TokenSalePage extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      subscribeURL: '//blockstack.us14.list-manage.com/subscribe/post?u=394a2b5cfee9c4b0f7525b009&amp;id=0e5478ae86',
+      videoURL: 'https://www.youtube.com/embed/Z4bMFKBRg_k'
+    }
   }
 
   render() {
-    const subscribeURL = '//blockstack.us14.list-manage.com/subscribe/post?u=394a2b5cfee9c4b0f7525b009&amp;id=0e5478ae86'
-
     return (
       <DocumentTitle title="Blockstack - Token Sale">
         <div className="token-hero">
@@ -26,12 +30,13 @@ class TokenSalePage extends Component {
               <section className="text-xs-center">
                 <Image className="stacks-logo-sm m-t-85 m-b-55"
                         src="/images/logos/stacks-logo.svg"
+                        fallbackSrc="/images/logos/stacks-logo.svg"
                         retinaSupport={false} />
                 <h1 className="text-white m-b-20">The Blockstack Token Sale</h1>
                 <p className="hero-lead purple-50">Sign up to receive important information about the token sale</p>
                 <div className="row">
                   <div className="col-md-9 col-centered m-t-2 m-b-3">
-                    <DarkForm subscribeURL={subscribeURL} submitButtonText='Sign Up' />
+                    <DarkForm subscribeURL={this.state.subscribeURL} submitButtonText='Sign Up' />
                   </div>
                 </div>
                 <p className="text-xs-center m-t-2 m-b-150">
@@ -57,6 +62,13 @@ class TokenSalePage extends Component {
                     6,574 
                     <span className="text-stats-description">community devs</span>
                   </div>
+                </section>
+              </div>
+            </div>
+            <div className="section-stats container-fluid" style={{ backgroundColor: '#DDD' }}>
+              <div className="container">
+                <section>
+                  <EmbedYouTube src={this.state.videoURL} />
                 </section>
               </div>
             </div>
