7c0ab359dac83a7be6f830ba10aa6506cd881604
Jack Zampolin <jack.zampolin@gmail.com>
2017-08-01 08:07:28 -0700

    Fix FAQ link
    This fixes the broken FAQ link.

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -56,7 +56,7 @@ export default (
       <Route path="/videos/:slug" component={TalkPage} />
 
       <Route path="/about" component={AboutPage} />
-      <Route path="/faq" component={FAQPage} />
+      <Route path="/faqs" component={FAQPage} />
       <Route path="/jobs" component={JobsPage} />
 
       <Route path="/docs" component={DocsPage} />
