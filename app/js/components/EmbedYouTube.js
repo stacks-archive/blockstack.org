b0bcd9fc578fd6bd05004a42cb9470cf69eb0bb1
arisgk <aris.gk@gmail.com>
2017-08-10 18:52:16 +0300

    Use Bootsrap classes to correct tutorial videos aspect ratio

--- app/js/components/EmbedYouTube.js
+++ app/js/components/EmbedYouTube.js
@@ -1,46 +1,25 @@
-import { Component, PropTypes } from 'react'
+import { PropTypes } from 'react'
 
 const propTypes = {
   src: PropTypes.string.isRequired,
 }
 
-class EmbedYouTube extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      height: 440
-    }
-  }
-
-  componentDidMount() {
-    const ratio = 440/672
-    const iframeWrapper = this.refs.iframeWrapper
-    const width = iframeWrapper.getBoundingClientRect().width
-    const newHeight = width*ratio
-    this.setState({
-      height: newHeight
-    })
-  }
-
-  render() {
-    return (
-      <div className="media-screen">
-        <div className="container">
-          <iframe
-            ref="iframeWrapper"
-            width="100%"
-            height={this.state.height}
-            src={this.props.src}
-            frameBorder="0"
-            allowFullScreen>
-          </iframe>
-        </div>
+const EmbedYouTube = ({ src }) => (
+  <div className="media-screen">
+    <div className="container no-padding">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          ref="iframeWrapper"
+          className="embed-responsive-item"
+          src={src}
+          frameBorder="0"
+          allowFullScreen>
+        </iframe>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+)
 
 EmbedYouTube.propTypes = propTypes
+
 export default EmbedYouTube
