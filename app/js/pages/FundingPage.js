67b810432514aa50255f710f342eb139a4e68018
Guy Lepage <guylepage3@gmail.com>
2017-08-16 04:42:19 -0400

    added terms section to fund page

--- app/js/pages/FundingPage.js
+++ app/js/pages/FundingPage.js
@@ -49,10 +49,13 @@ class FundingPage extends Component {
                 <Image className="blockstack-signature-fund-logo-md m-t-55 m-b-45"
                   src="/images/logos/blockstack-signature-fund-logo-rev.svg"
                   retinaSupport={false} />
-                { /*<h1 className="text-white m-b-20">Blockstack Signature Fund</h1> */ }
-                <p className="hero-lead text-white p-b-30">“Money is bringing everybody in, but the thing we are creating is larger than ourselves, < br/>
-                larger than money. “ - Naval Ravikant at Blockstack Summit 2017</p>
-                <div className="col-sm-9 col-md-7 col-centered m-b-45">
+                <p className="lead-lg text-white col-md-8 col-centered p-b-10">
+                  "Money is bringing everybody in, but the thing we are creating is larger than ourselves, larger than money."
+                </p>
+                <p className="hero-lead text-white p-b-30">
+                  — Naval Ravikant at Blockstack Summit 2017
+                </p>
+                <div className="col-sm-9 col-md-7 col-centered m-b-65">
                   <Link to="/whitepaper.pdf" target="_blank" className="btn btn-electric-blue btn-block">
                     Apply Here!
                   </Link>
@@ -126,7 +129,7 @@ class FundingPage extends Component {
                     <h3 className="text-xs-center m-b-45">
                       Why Apply?
                     </h3>
-                    <p className="lead-lg text-xs-center">
+                    <p className="lead-lg text-xs-center col-md-10 col-centered">
                       These is an early step to building a sustainable, responsible ecosystem.
                       Just like building skyscraper, we are now pouring the concrete foundation.
                       There will be many more layers to come.
@@ -157,6 +160,81 @@ class FundingPage extends Component {
                     </p>
                     <p className="lead-lg text-xs-center">
                       Members of the Blockstack team will reach out to you if there’s interest in working more closely to build a team (if needed) and a product using Blockstack. Projects will be selected for funding consideration and support.
+                    </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="sectionContainerLightGray section-spacing container-fluid">
+              <section>
+                <div className="containWrapper">
+                  <div className="container sectionWrap">
+                    <div className="col-md-10 col-centered">
+                    <h3 className="text-xs-center m-b-45">
+                      What is Blockstack?
+                    </h3>
+                    <p className="lead-lg text-xs-center col-md-10 col-centered">
+                      Blockstack is a new fully decentralized internet that comes with a full stack of open-source developer tools to build and bootstrap decentralized applications and protocol ecosystems.
+                    </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="sectionContainerLight section-spacing container-fluid">
+              <section>
+                <div className="containWrapper">
+                  <div className="container sectionWrap">
+                    <div className="col-md-10 col-centered">
+                    <p className="lead-lg text-xs-center col-md-10 col-centered m-b-45">
+                      In Partnership with:
+                    </p>
+                    <div className="col-sm-9 col-md-7 col-centered m-b-45">
+                      <Link to="/whitepaper.pdf" target="_blank" className="btn btn-electric-blue btn-block">
+                        Apply Here!
+                      </Link>
+                    </div>
+                    <p className="lead-lg text-xs-center">
+                      Once you apply, we’ll provide you with some starter materials to get started building apps on Blockstack.
+                    </p>
+                    <p className="lead-lg text-xs-center">
+                      Members of the Blockstack team will reach out to you if there’s interest in working more closely to build a team (if needed) and a product using Blockstack. Projects will be selected for funding consideration and support.
+                    </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="sectionContainerLightGray section-spacing container-fluid">
+              <section>
+                <div className="containWrapper">
+                  <div className="container sectionWrap">
+                    <div className="col-md-9 col-centered">
+                    <p className="lead-lg">
+                      Terms & Conditions
+                    </p>
+                    <p className="terms-sm">
+                      Thank you for your interest in Signature Fund. By submitting any information or materials to Blockstack regarding the Signature Fund program and application process (the "Program"), you (and the entity or business you represent) (collectively, "you") agree to be bound by the following terms:
+                    </p>
+                    <p className="terms-sm">
+                      <ul>
+                        <li>
+                          You acknowledge and agree that Blockstack will share all information you provide in connection with the Program(including product usage and spend data), to our VC partners in Signature Fund.
+                        </li>
+                        <li>
+                          You should not submit or provide anything you consider confidential or proprietary as part of the Program. Due to the large number of submissions we receive, we cannot be responsible for maintaining submitted information or materials in confidence. Accordingly, you represent and agree that all information you provide in connection with the Program will be non-confidential, and you acknowledge and agree that such information will not be held in confidence or restricted from use or disclosure in any way.
+                        </li>
+                        <li>
+                          There are a large number of applicants for Signature Fund. Accordingly, we make no promises or guarantees that you will receive funding or even be invited to apply. Blockstack, Signature Fund and its VC partners have no obligation or duty to you unless and until a definitive agreement regarding funding is executed and delivered by all relevant parties (if at all).
+                        </li>
+                        <li>
+                          NOTWITHSTANDING ANYTHING TO THE CONTRARY, Blockstack, THE Signature Fund AND ITS VC PARTNERS WILL NOT BE RESPONSIBLE OR LIABLE, UNDER ANY LEGAL OR EQUITABLE THEORY, FOR ANY INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OF ANY KIND OR FOR ANY AMOUNT IN THE AGGREGATE IN EXCESS OF FIFTY DOLLARS ($50).
+                        </li>
+                        <li>
+                          These Signature Fund Submission Terms are governed by the laws of the State of New York.
+                        </li>
+                      </ul>
                     </p>
                     </div>
                   </div>
