5e05d6b659b5dfa1df1d64fc5bf15ccc2f090703
Guy Lepage <guylepage3@gmail.com>
2017-08-16 04:16:39 -0400

    added section 3 of fund page

--- app/js/pages/FundingPage.js
+++ app/js/pages/FundingPage.js
@@ -91,15 +91,28 @@ class FundingPage extends Component {
                 <div className="containWrapper">
                   <div className="container sectionWrap">
                     <div className="col-md-10 col-centered">
-                    <h3 className="text-xs-center m-b-65">
-                      The Signature Fund for Blockstack is a $25M fund aimed at growing an ecosystem of decentralized applications on Blockstack.
-                    </h3>
-                    <p className="lead-lg text-xs-center">
-                      We partnered with thesis-driven VC’s thesis-driven VC's who're aligned with our vision of a decentralized internet like Lux Capital, Rising Tide, Compound, OpenOcean, and VersionOne to fund app developers and teams looking to build their apps on the new fully decentralized internet. App creators will benefit directly from the deep expertise and insight of these experienced investors.
-                    </p>
-                    <p className="lead-lg text-xs-center">
-                      Additionally, those who apply will be first to hear 'x-prize' style prizes/bounties for large initiatives like building a decentralized microblogging platform, funded by investors like Naval Ravikant (founder, AngelList), Albert Wenger (managing partner, USV), and more
-                    </p>
+                      <h3 className="text-xs-center">
+                        “Less white paper hype, more working code.”
+                      </h3>
+                      <p className="hero-lead m-b-65">
+                        — Jeremy Welch, Casa App Founder at Blockstack Summit 2017
+                      </p>
+                      <p className="lead-lg text-xs-center m-b-45">
+                        There’s a lot of excitement about building decentralized applications, but we know it is still early days. At Blockstack we’ve created the core infrastructure to enable developers to build these applications, but we realize that to increase an app’s chances of success, it will require access to capital and tools to bootstrap networks.
+                      </p>
+                      <p className="lead-lg m-b-25">
+                         We think in the long term it’s not just about raising a round or a token, but more about forging an ecosystem that keeps your application thriving. Here are just a few of the methods opening up to applications being built on Blockstack:
+                      </p>
+                      <p className="lead-lg col-md-10 col-centered m-b-65">
+                        <ul>
+                          <li>VC-led Funding: Blockstack Apps will get access to and capital from some of the best minds in the space.</li>
+                          <li>Token: Blockstack Apps will be able to offer tokens on top of the Blockstack ecosystem, just like ERC20 tokens on Ethereum.</li>
+                          <li>Advanced Payments: Forward a percentage of your app’s subscription fees to specific names/addresses to incentivize app-building behavior.</li>
+                        </ul>
+                      </p>
+                      <h4 className="font-weight-bold text-xs-center">
+                        These are just the first steps to building an app ecosystem for the decentralized internet, and we expect tools to evolve and capabilities to grow.
+                      </h4>
                     </div>
                   </div>
                 </div>
