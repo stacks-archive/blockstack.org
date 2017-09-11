a19b661e7b07446332deca5deb9088a69def830c
estermer <eric.stermer@gmail.com>
2017-09-11 11:35:21 -0400

    syntax corrections and package-lock file added back in

--- app/js/pages/FAQPage.js
+++ app/js/pages/FAQPage.js
@@ -22,7 +22,7 @@ marked.setOptions({
 
 class FAQpage extends Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
@@ -45,7 +45,7 @@ class FAQpage extends Component {
     for (let i = 0; i < markdownParts.length; i += 2) {
       questions.push({
         question: markdownParts[i],
-        answer: marked(markdownParts[i + 1])
+        answer: marked(markdownParts[i+1])
       })
     }
 
@@ -58,48 +58,46 @@ class FAQpage extends Component {
     return (
       <DocumentTitle title="Blockstack - FAQ ">
         <div>
-          <div>
-            <TokenBanner />
-            <Header />
-            <div className='sidebar-wrapper'>
-              <div className="pull-left faq-sidebar">
-                <div className="list-group">
-                  <h5 className="list-group-header">Frequently Asked Questions</h5>
-                  { questions.map((faq, index) => {
+          <TokenBanner />
+          <Header />
+          <div className='sidebar-wrapper'>
+            <div className="pull-left faq-sidebar">
+              <div className="list-group">
+                <h5 className="list-group-header">Frequently Asked Questions</h5>
+                { questions.map((faq, index) => {
+                  const refLink = faq.question.toLowerCase().split(' ').join('_');
+                  return (
+                    <Link key={index}
+                          href={`/faq/#${refLink}`}
+                          className="list-group-item">
+                      {faq.question}
+                    </Link>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="sidebar-content-wrapper">
+            <section className="container-fluid spacing-container">
+              <div className="container-fluid col-centered segment-wrapper">
+                <div className="m-x-2">
+                  <div>
+                    <h1>Frequently Asked Questions</h1>
+                  </div>
+                  {questions.map((faq, index) => {
                     const refLink = faq.question.toLowerCase().split(' ').join('_');
                     return (
-                      <Link key={index}
-                            href={`/faq/#${refLink}`}
-                            className="list-group-item">
-                        {faq.question}
-                      </Link>
+                      <div key={index} id={`${refLink}`}>
+                        <h4>{faq.question}</h4>
+                        <div dangerouslySetInnerHTML={{
+                          __html: faq.answer
+                        }}></div>
+                      </div>
                     )
                   })}
                 </div>
               </div>
-            </div>
-            <div className="sidebar-content-wrapper">
-              <section className="container-fluid spacing-container">
-                <div className="container-fluid col-centered segment-wrapper">
-                  <div className="m-x-2">
-                    <div>
-                      <h1>Frequently Asked Questions</h1>
-                    </div>
-                    {questions.map((faq, index) => {
-                      const refLink = faq.question.toLowerCase().split(' ').join('_');
-                      return (
-                        <div key={index} id={`${refLink}`}>
-                          <h4>{faq.question}</h4>
-                          <div dangerouslySetInnerHTML={{
-                            __html: faq.answer
-                          }}></div>
-                        </div>
-                      )
-                    })}
-                  </div>
-                </div>
-              </section>
-            </div>
+            </section>
           </div>
         </div>
       </DocumentTitle>
