83ef960bc3e681202cd518215fdd6f8a6d7203bc
Guy Lepage <guylepage3@gmail.com>
2017-09-26 11:31:45 -0400

    new close button primary

--- app/js/components/Alert.js
+++ app/js/components/Alert.js
@@ -13,7 +13,7 @@ class Alert extends Component {
     return (
       <Link to="/token" className="alert alert-primary alert-dismissible fade show text-center text-white" style={{ marginBottom: '0' }}>
         <div style={{ marginLeft: "26px" }}>
-          <button type="button" className="close" data-dismiss="alert" aria-label="Close" style={{ top: '-.65rem' }}>
+          <button type="button" className="close close-primary" data-dismiss="alert" aria-label="Close" style={{ top: '-.65rem' }}>
             <span aria-hidden="true">&times;</span>
           </button>
           Introducing The <strong>Blockstack Token</strong> &nbsp;&nbsp; › &nbsp; <strong>More</strong>
