dc91de97392e4f5d47c1c03a05f9d78ed98389d1
Guy Lepage <guylepage3@gmail.com>
2017-09-26 05:57:18 -0400

    removed test navbar

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -36,30 +36,6 @@ class Header extends Component {
     return (
       <div className={ this.props.transparent ? 'bg-transparent' : 'bg-primary'}>
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-
           <nav className="navbar navbar-expand-lg">
             <Link className="navbar-brand brand-bug" to="/">
               <img src="/images/logos/blockstack-bug-rev.svg" />
@@ -112,7 +88,6 @@ class Header extends Component {
               })}
             </ul>
           </div>
-
 
       </div>
 
