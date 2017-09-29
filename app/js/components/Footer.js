a8c432bb3de19ced4ecc31363ccaa11d21795faa
Guy Lepage <guylepage3@gmail.com>
2017-09-29 08:53:52 -0400

    resolved mobile tablet desktop footer main nav

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -17,85 +17,90 @@ class Footer extends Component {
       <footer>
         <div className="container">
           <div className="row">
-            <div className="col-md-2 foot-left-col d-none d-lg-block">
+            <div className="d-none d-lg-inline">
               <div className="foot-logo">
                 <Link to="/">
                   <img src="/images/logos/blockstack-logo-landscape-rev.svg" />
                 </Link>
               </div>
             </div>
-            <div className="col-6 col-lg-2">
-              <div className="foot-menu">
-                <div className="foot-item text-white">
-                  Pages
+            <div className="container-fluid foot-3col">
+              <div className="row">
+                <div className="col-6 col-lg-3">
+                  <div className="foot-menu">
+                    <div className="foot-item text-white">
+                      Pages
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/intro">Intro</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/tutorials">Tutorials</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/blog">Blog</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/papers">Papers</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/videos">Videos</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/about">About</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/faq">FAQs</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/careers">Careers</Link>
+                    </div>
+                  </div>
                 </div>
-                <div className="foot-item">
-                  <Link to="/intro">Intro</Link>
+                <div className="col-6 col-lg-3">
+                  <div className="foot-menu">
+                    <div className="foot-item text-white">
+                      Community
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.forum} target="_blank">Forum</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.slack} target="_blank">Slack</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.meetup} target="_blank">Meetup</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.youtube} target="_blank">YouTube</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/summit2017">Summit 2017</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to="/funding">Funding</Link>
+                    </div>
+                  </div>
                 </div>
-                <div className="foot-item">
-                  <Link to="/tutorials">Tutorials</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/blog">Blog</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/papers">Papers</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/videos">Videos</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/about">About</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/faq">FAQs</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/careers">Careers</Link>
+                <div className="col-6 col-lg-3">
+                  <div className="foot-menu">
+                    <div className="foot-item text-white">
+                      Resources
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.github} target="_blank">Github</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.twitter} target="_blank">Twitter</Link>
+                    </div>
+                    <div className="foot-item">
+                      <Link to={links.branding} target="_blank">Branding</Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-6 col-lg-2">
-              <div className="foot-menu">
-                <div className="foot-item text-white">
-                  Community
-                </div>
-                <div className="foot-item">
-                  <Link to={links.forum} target="_blank">Forum</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to={links.slack} target="_blank">Slack</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to={links.meetup} target="_blank">Meetup</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to={links.youtube} target="_blank">YouTube</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/summit2017">Summit 2017</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to="/funding">Funding</Link>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 col-lg-2">
-              <div className="foot-menu">
-                <div className="foot-item text-white">
-                  Resources
-                </div>
-                <div className="foot-item">
-                  <Link to={links.github} target="_blank">Github</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to={links.twitter} target="_blank">Twitter</Link>
-                </div>
-                <div className="foot-item">
-                  <Link to={links.branding} target="_blank">Branding</Link>
-                </div>
-              </div>
-            </div>
+
           </div>
         </div>
       </footer>
