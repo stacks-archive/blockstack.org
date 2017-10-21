2c723c30a95688b6619bdf6f71afc43b3113f125
Guy Lepage <guylepage3@gmail.com>
2017-10-20 21:26:11 -0400

    update to token paper link token page #447

--- app/js/pages/TokenSalePage.js
+++ app/js/pages/TokenSalePage.js
@@ -110,7 +110,7 @@ class TokenSalePage extends Component {
                         <h3 className="text-white m-b-30">
                           Blockstack Token Whitepaper
                         </h3>
-                        <a href="/token-whitepaper.pdf" role="button"
+                        <a href="/tokenpaper.pdf" role="button"
                           className="btn btn-outline-light btn-pill" style={{ minWidth: '245px' }}>
                           Read the Whitepaper
                         </a>
