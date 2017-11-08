a099b435f3f68f9da4d47baab24e4ce038dee0bf
Guy Lepage <guylepage3@gmail.com>
2017-11-08 11:39:46 -0500

    new countdown timer banner #459

--- app/js/components/Alert.js
+++ app/js/components/Alert.js
@@ -11,12 +11,15 @@ class Alert extends Component {
 
   render() {
     return (
-      <a href="http://blockstack.com" target="_blank" className="alert alert-primary alert-dismissible fade show text-center text-white" role="alert" style={{ marginBottom: '0', display: "block" }}>
+      <a href="http://blockstack.com" target="_blank" className="alert alert-primary alert-dismissible fade show text-center text-white" role="alert" style={{ marginBottom: '0', display: "block", paddingBottom: '21px' }}>
         <div>
           <button type="button" className="close close-primary d-none d-sm-block" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <span style={{ marginLeft: "26px" }}>Blockstack Token Sale Registration Now Open &nbsp;&nbsp; › &nbsp; <strong>Register Now</strong></span>
+          <span className="" style={{ marginLeft: "26px" }}>Blockstack Token Sale Registration Now Open &nbsp;&nbsp; › &nbsp; <strong>Register Now</strong></span>
+          <span className="banner-timer-wrap">
+            <span className="banner-time">03</span> days <span className="banner-time">12</span> hrs
+          </span>
         </div>
       </a>
     )
