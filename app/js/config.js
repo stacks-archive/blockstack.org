9f9704992e271983ee0e6e4debc05bb9d845931e
Ken <yukanliao@gmail.com>
2017-11-08 17:57:01 -0500

    Update browser download links.

--- app/js/config.js
+++ app/js/config.js
@@ -15,9 +15,10 @@ export const socialLinks = {
 export const installationLinks = {
   portalMacOS: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.9.0/Blockstack-v0.9.app.zip',
   portalDevelopers: 'https://github.com/blockstack/blockstack-browser/releases',
-  portalDevelopersMacOS: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.20.1/Blockstack-for-macOS-v0.20.1.dmg',
-  portalDevelopersWindows: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.20.1/Blockstack-for-win10-v0.20.1.msi',
-  portalDevelopersLinux: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.20.1/Blockstack-for-Linux-v0.20.1.sh',
+  portalDevelopersMacOS: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.21.0/Blockstack-for-macOS-v0.21.0.dmg',
+  portalDevelopersWindows: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.21.0/Blockstack-for-win10-v0.21.0.msi',
+  portalDevelopersLinux: 'https://github.com/blockstack/blockstack-browser/releases/download/v0.21.0/Blockstack-for-Linux-v0.21.0.sh',
+  portalDevelopersWeb: 'https://browser.blockstack.org',
   cliDevelopers: 'https://github.com/blockstack/blockstack-core',
 }
 
