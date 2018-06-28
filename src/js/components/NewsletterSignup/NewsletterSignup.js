fcecdf4968ebb0b32e3dec96b0066c4ce1f17d97
Robin Willis <robincwillis@gmail.com>
2018-06-28 16:29:07 -0400

    add mailchimp newsletter signup

--- src/js/components/NewsletterSignup/NewsletterSignup.js
+++ src/js/components/NewsletterSignup/NewsletterSignup.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import InlineSVG from 'svg-inline-react';
+import toQueryString from "to-querystring";
+import jsonp from "jsonp";
 
 import Input from 'js/components/Input';
 import Button from 'js/components/Button';
@@ -12,7 +14,9 @@ class NewsletterSignup extends Component {
 
 	state = {
 		email : '',
-		validEmail : true
+		validEmail : true,
+		success : false,
+		error : null
 	}
 
 	updateEmailAddress = (event) => {
@@ -26,12 +30,44 @@ class NewsletterSignup extends Component {
 
 	}
 
+	signup = (event) => {
+
+
+		console.log('sign me up please');
+
+		const options = {
+			method : 'POST',
+			headers : { 'Content-type': 'application/json; charset=UTF-8',}
+		};
+
+		const data = { EMAIL : this.state.email};
+
+		console.log(process.env);
+
+		// TODO : GET this from mail chimp
+		const url = process.env.MAILCHIMP_URL.replace("/post?", "/post-json?");
+
+		const params = toQueryString(data);
+
+
+		jsonp(url + "&" + params, {param: "c" }, (err, res) => {
+			if (err || res.result !== "success") {
+		    console.log(err);
+		    this.setState({error : err});
+		  } else {
+		  	console.log(res);
+		  	this.setState({success : true});
+		  }
+		});
+
+	}
+
 	render() {
 		return (
-			<form className={!this.state.validEmail && this.state.email != '' ? ('newsletter-form invalid') : ('newsletter-form')}>
+			<div className={!this.state.validEmail && this.state.email != '' ? ('newsletter-form invalid') : ('newsletter-form')}>
 				<Input id={this.props.id} placeholder="Get email updates" className={this.props.inputClass} value={this.state.email} onChange={this.updateEmailAddress} />
-				<Button icon={Arrow()} className={"transparent circle " + this.props.inputClass} htmlFor={this.props.id} disabled={this.state.email.length < 4 || !this.state.validEmail ? true : false} />
-			</form>
+				<Button onClick={this.signup} icon={Arrow()} className={"transparent circle " + this.props.inputClass} htmlFor={this.props.id} disabled={this.state.email.length < 4 || !this.state.validEmail || this.state.success ? true : false} />
+			</div>
 		);
 	}
 }
