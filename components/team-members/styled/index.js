36e5fcd5052199497f9981ba80253270f4c7ef93
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-07-16 18:42:42 -0400

    fix team members on mobile

--- components/team-members/styled/index.js
+++ components/team-members/styled/index.js
@@ -7,13 +7,13 @@ const StyledTeamMembers = styled.div`
 `
 
 const Avatar = styled.div`
-img, picture{
-  max-width: 100px;
-  display: block;
-  overflow: hidden;
-  border-radius: 100%;
-}
-
+  img,
+  picture {
+    max-width: 100px;
+    display: block;
+    overflow: hidden;
+    border-radius: 100%;
+  }
 `
 
 const StyledTeamMember = styled.div`
@@ -25,6 +25,13 @@ const StyledTeamMember = styled.div`
   flex-direction: column;
   min-width: 380px;
   padding: 20px 40px 100px;
+  @media (max-width: 600px) {
+    padding: 20px 0 60px;
+    min-width: 100%;
+    p {
+      max-width: 100%;
+    }
+  }
   h3,
   h5,
   h6 {
