b8a1ba5ffeac61c6d27c7888945ced392db5d398
William O'Beirne <wbobeirne@gmail.com>
2018-07-26 13:22:38 -0400

    Change out videos.

--- pages/videos/data.js
+++ pages/videos/data.js
@@ -19,26 +19,26 @@ export const videoData = [
     urlSlug: 'the-conference-the-future-of-the-internet-is-decentralized',
     image: 'images/videos/the_future_is_decentralized.jpg'
   },
-  {
-    title: 'A New Blockstack Internet',
-    event: 'Blockstack Summit 2017',
-    speaker: 'Ryan Shea',
-    location: 'Mountain View, CA',
-    date: 'July 27, 2017',
-    youtubeURL: 'https://www.youtube.com/watch?v=YzlyEuRfXxo',
-    urlSlug: 'blockstack-summit-2017-a-new-blockstack-internet',
-    image: 'images/videos/a_new_blockstack_internet.jpg'
-  },
-  {
-    title: 'The Next Wave of Computing',
-    event: 'Blockstack Summit 2017',
-    speaker: 'Muneeb Ali',
-    location: 'Mountain View, CA',
-    date: 'July 27, 2017',
-    youtubeURL: 'https://www.youtube.com/watch?v=IfONVXL6tnk',
-    urlSlug: 'blockstack-summit-2017-the-next-wave-of-computing',
-    image: 'images/videos/the_next_wave_of_computing.jpg'
-  },
+  // {
+  //   title: 'A New Blockstack Internet',
+  //   event: 'Blockstack Summit 2017',
+  //   speaker: 'Ryan Shea',
+  //   location: 'Mountain View, CA',
+  //   date: 'July 27, 2017',
+  //   youtubeURL: 'https://www.youtube.com/watch?v=YzlyEuRfXxo',
+  //   urlSlug: 'blockstack-summit-2017-a-new-blockstack-internet',
+  //   image: 'images/videos/a_new_blockstack_internet.jpg'
+  // },
+  // {
+  //   title: 'The Next Wave of Computing',
+  //   event: 'Blockstack Summit 2017',
+  //   speaker: 'Muneeb Ali',
+  //   location: 'Mountain View, CA',
+  //   date: 'July 27, 2017',
+  //   youtubeURL: 'https://www.youtube.com/watch?v=IfONVXL6tnk',
+  //   urlSlug: 'blockstack-summit-2017-the-next-wave-of-computing',
+  //   image: 'images/videos/the_next_wave_of_computing.jpg'
+  // },
   {
     title: 'A Conversation with Naval Ravikant and Ryan Shea',
     event: 'Blockstack Summit 2017',
@@ -50,30 +50,30 @@ export const videoData = [
       'blockstack-summit-2017-a-conversation-with-naval-ravikant-and-ryan-shea',
     image: 'images/videos/convo_naval_ryan.jpg'
   },
-  {
-    title:
-      '"A New Decentralized App Ecosystem on Blockstack" | Blockstack Summit 2017',
-    event: 'Blockstack Summit 2017',
-    speaker: 'Patrick Stanley',
-    location: 'Mountain View, CA',
-    date: 'July 27, 2017',
-    youtubeURL: 'https://www.youtube.com/watch?v=m3pwRj68zQ0',
-    urlSlug:
-      'blockstack-summit-2017-a-new-decentralized-app-ecosystem-on-blockstack-blockstack-summit-2017',
-    image: 'images/videos/summit_2017.jpg'
-  },
-  {
-    title:
-      "Q&A w/ Blockstack's Muneeb Ali & Ryan Shea, Moderated by Alice Lloyd George",
-    event: 'Blockstack Summit 2017',
-    speaker: 'Muneeb Ali, Ryan Shea, Alice Lloyd George',
-    location: 'Mountain View, CA',
-    date: 'July 27, 2017',
-    youtubeURL: 'https://www.youtube.com/watch?v=JY5EMjHe4-Y',
-    urlSlug:
-      'blockstack-summit-2017-qa-w-blockstacks-muneeb-ali-ryan-shea-moderated-by-alice-lloyd-george',
-    image: 'images/videos/q_a_ryan_muneeb.jpg'
-  },
+  // {
+  //   title:
+  //     '"A New Decentralized App Ecosystem on Blockstack" | Blockstack Summit 2017',
+  //   event: 'Blockstack Summit 2017',
+  //   speaker: 'Patrick Stanley',
+  //   location: 'Mountain View, CA',
+  //   date: 'July 27, 2017',
+  //   youtubeURL: 'https://www.youtube.com/watch?v=m3pwRj68zQ0',
+  //   urlSlug:
+  //     'blockstack-summit-2017-a-new-decentralized-app-ecosystem-on-blockstack-blockstack-summit-2017',
+  //   image: 'images/videos/summit_2017.jpg'
+  // },
+  // {
+  //   title:
+  //     "Q&A w/ Blockstack's Muneeb Ali & Ryan Shea, Moderated by Alice Lloyd George",
+  //   event: 'Blockstack Summit 2017',
+  //   speaker: 'Muneeb Ali, Ryan Shea, Alice Lloyd George',
+  //   location: 'Mountain View, CA',
+  //   date: 'July 27, 2017',
+  //   youtubeURL: 'https://www.youtube.com/watch?v=JY5EMjHe4-Y',
+  //   urlSlug:
+  //     'blockstack-summit-2017-qa-w-blockstacks-muneeb-ali-ryan-shea-moderated-by-alice-lloyd-george',
+  //   image: 'images/videos/q_a_ryan_muneeb.jpg'
+  // },
   // {
   //   title:
   //     'Blockstack: A New Internet That Brings Privacy & Property Rights to Cyberspace',
@@ -137,15 +137,15 @@ export const videoData = [
   //   urlSlug: 'decentralized-apps-and-sovereign-identity',
   //   image: 'https://i.ytimg.com/vi/dCNC2SjqYHc/maxresdefault.jpg'
   // },
-  // {
-  //   title: 'Welcome to the New Internet',
-  //   event: 'TEDxNewYork',
-  //   speaker: 'Muneeb Ali',
-  //   location: 'New York, NY',
-  //   date: 'September 10, 2016',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=qtOIh93Hvuw',
-  //   speakerDeckID: '15d4861b7c1842bfbd7add80c99e1cf6',
-  //   urlSlug: 'welcome-to-the-new-internet',
-  //   image: 'https://i.ytimg.com/vi/qtOIh93Hvuw/maxresdefault.jpg'
-  // }
+  {
+    title: 'Welcome to the New Internet',
+    event: 'TEDxNewYork',
+    speaker: 'Muneeb Ali',
+    location: 'New York, NY',
+    date: 'September 10, 2016',
+    youtubeURL: 'https://www.youtube.com/watch?v=qtOIh93Hvuw',
+    speakerDeckID: '15d4861b7c1842bfbd7add80c99e1cf6',
+    urlSlug: 'welcome-to-the-new-internet',
+    image: 'https://i.ytimg.com/vi/qtOIh93Hvuw/maxresdefault.jpg'
+  }
 ]
