3b378d928972b4a935c91f5ea87885814d46e7cc
William O'Beirne <wbobeirne@gmail.com>
2018-07-26 14:04:34 -0400

    Remove unneeded redux-bundler bundles.

--- bundles/index.js
+++ bundles/index.js
@@ -1,8 +1,7 @@
-import { composeBundles, createUrlBundle } from 'redux-bundler'
+import { composeBundlesRaw, debugBundle } from 'redux-bundler'
 
 import blog from './blog'
 import jobs from './jobs'
 import ui from './ui'
-import url from './url'
 
-export default composeBundles(blog, jobs, ui, url)
+export default composeBundlesRaw(blog, jobs, ui, debugBundle)
