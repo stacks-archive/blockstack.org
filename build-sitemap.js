84a583395fc9356fd621b6ac2b655dfa2215174e
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-08-24 10:38:26 +0800

    improve sitemap script, remove &&

--- build-sitemap.js
+++ build-sitemap.js
@@ -10,100 +10,40 @@ let xml = ''
 xml += `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://blockstack.org/</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/about</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/blog</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/faq</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/careers</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/funding</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/install</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/papers</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/press</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/videos</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/what-is-blockstack</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/legal/disclaimers</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/legal/privacy-policy</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/legal/terms-of-use</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/tutorials</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/tutorials/hello-blockstack</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/tutorials/multi-player-storage</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>https://blockstack.org/tutorials/todo-list</loc>
-        <changefreq>always</changefreq>
-        <priority>0.5</priority>
-    </url>
 `
+
+const staticPaths = [
+  '/',
+  '/about',
+  '/blog',
+  '/faq',
+  '/careers',
+  '/funding',
+  '/install',
+  '/papers',
+  '/press',
+  '/videos',
+  '/what-is-blockstack',
+  '/legal/disclaimers',
+  '/legal/privacy-policy',
+  '/legal/terms-of-use',
+  '/tutorials',
+  '/tutorials/hello-blockstack',
+  '/tutorials/multi-player-storage',
+  '/tutorials/todo-list'
+]
 
 fetchBlogPosts().then((data) => {
   const { posts } = data
+
+  staticPaths.forEach((path) => {
+    xml += '<url>'
+    xml += `<loc>https://blockstack.org${path}</loc>`
+    xml += `<changefreq>always</changefreq>`
+    xml += `<priority>0.5</priority>`
+    xml += '</url>'
+  })
+
   posts.forEach((post) => {
     xml += '<url>'
     xml += `<loc>https://blockstack.org/blog/${post.urlSlug}</loc>`
