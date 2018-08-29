110e5c5ad89d30187a7f2efab23bfe1a760cc3f7
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-08-29 20:03:00 +0900

    refine header colors/transitions a bit more

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,27 @@ import { ServerStyleSheet, injectGlobal } from 'styled-components'
 import { GA_TRACKING_ID } from '@common/lib/gtag'
 
 injectGlobal`
+.header{
+  transition: 0.25s all ease-in-out;
+  svg .logo-bg{
+    transition: 0.25s all ease-in-out;
+  }
+}
 .headroom.headroom--scrolled{
-box-shadow: 0 3px 12px rgba(37,0,105,.32);
+  box-shadow: 0 3px 12px rgba(37,0,105,.32);
+  .header{
+    background:#211f6d;
+    svg .logo-bg{
+      fill: transparent!important;
+    }   
+  }
 }`
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
     const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App { ...props } />)
     )
     const styleTags = sheet.getStyleElement()
     return { ...page, styleTags }
@@ -22,30 +34,30 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
-          {this.props.styleTags}
-          <title>Blockstack</title>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+      <Head>
+        <link rel="stylesheet" href="/_next/static/style.css"/>
+        { this.props.styleTags }
+        <title>Blockstack</title>
+        { /* Global Site Tag (gtag.js) - Google Analytics */ }
+        <script
+          async
+          src={ `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}` }
+        />
+        <script
+          dangerouslySetInnerHTML={ {
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_TRACKING_ID}');
           `
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+          } }
+        />
+      </Head>
+      <body>
+      <Main/>
+      <NextScript/>
+      </body>
       </html>
     )
   }
