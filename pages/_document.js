54c2d6a39031f10c6ad2a2038c5376cd38d1dd14
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-09-28 10:34:39 -0500

    remove title from _document

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,20 @@ injectGlobal`
 }
 .headroom.headroom--scrolled{
   box-shadow: 0 3px 12px rgba(37,0,105,.32);
+  .center-actions {
+    .button.main-action,
+    .newsletter-form {
+      opacity: 1;
+      visibility: visible;
+      transform: none;
+      svg {
+        * {
+          mix-blend-mode: screen;
+          fill: white !important;
+        }
+      }
+    }
+  }
   .header{
     background:#211f6d;
     svg .logo-bg{
@@ -25,7 +39,7 @@ export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
     const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App { ...props } />)
     )
     const styleTags = sheet.getStyleElement()
     return { ...page, styleTags }
@@ -34,28 +48,27 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>
-          {this.props.styleTags}
-          <title>Blockstack</title>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+      <Head>
+        { this.props.styleTags }
+        <script
+          async
+          src={ `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}` }
+        />
+        <script
+          dangerouslySetInnerHTML={ {
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_TRACKING_ID}');
           `
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+          } }
+        />
+      </Head>
+      <body>
+      <Main/>
+      <NextScript/>
+      </body>
       </html>
     )
   }
