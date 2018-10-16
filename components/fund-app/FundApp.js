75a1fa1a6460c9d066e790498330d40d6244c54a
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-16 12:25:19 -0500

    add App Mining link to Fund App homepage section #679

--- components/fund-app/FundApp.js
+++ components/fund-app/FundApp.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-
 class Header extends Component {
   render() {
     return (
@@ -8,11 +7,13 @@ class Header extends Component {
         <div className="container align-center">
           <h3 className="p main-color pb-3">Fund your dapp</h3>
           <p className="h2 main-color">
-            We have funding available to support teams via our{' '}
+            We have funding available to support teams via{' '}
+            <a href={this.props.links.appMining}>App Mining</a>,
             <a href={this.props.links.signatureFund}>Signature Fund</a>,{' '}
             <a href="https://contribute.blockstack.org/">
               Community Rewards Program
-            </a>, and <a href="/blog">Active Bounties</a>.
+            </a>
+            , and <a href="/blog">Active Bounties</a>.
           </p>
         </div>
       </section>
