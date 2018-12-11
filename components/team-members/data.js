31a0d169a9b2ebaab3947d989f2085137a9c7b1e
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-12-11 15:38:18 -0500

    adjust data for team members

--- components/team-members/data.js
+++ components/team-members/data.js
@@ -4,60 +4,29 @@ export const board = [
   {
     blockstackId: 'muneeb',
     name: 'Muneeb Ali',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
     bio:
       'Muneeb Ali is a distributed systems PhD from Princeton and gives guest lectures on cloud computing there. He went through Y Combinator and was awarded a J. William Fulbright Fellowship.'
   },
   {
-    name: 'Arvind Narayanan',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
+    name: 'Albert Wenger',
     bio:
-      'Arvind Narayanan is an Associate Professor at Princeton University and an Affiliate Scholar at the Stanford Law School. He researches information privacy, internet security, and has written a textbook on Bitcoin and cryptocurrency technologies. His work on internet privacy received the 2008 Privacy Enhancing Technologies Award.'
+      'Albert Wenger has served as a member of the board of directors since December 2016. Mr. Wenger is a managing partner at Union Square Ventures (USV), where he has led a variety of investments including USV’s investments in Etsy (IPO), MongoDB (IPO), Twilio (IPO),  Behance (acquired by Adobe), and Firebase (acquired by Google).'
   },
   {
-    name: 'Arianna Simpson',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
+    blockstackId: 'jpsingh',
+    name: 'JP Singh',
     bio:
-      'Arianna Simpson is an early stage investor and Managing Director at Autonomous Partners, an investment fund focused on cryptocurrencies and digital assets. Before she started investing, Arianna was at BitGo, a blockchain security company, where she was the third employee.'
-  },
-  {
-    name: 'Catherine Tucker',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
-    bio:
-      'Catherine Tucker is the Sloan Distinguished Professor of Management at MIT Sloan and Chair of the MIT Sloan PhD Program. She researches how technology can improve operations and marketing at firms, and associated challenges for regulations. She is associate editor at Management Science and a research associate at the National Bureau of Economic Research.'
-  },
-  {
-    name: 'Charlie Saravia',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
-    bio:
-      'Charlie Saravia is a Managing Director of HMC. Previously, Mr. Saravia was a Managing Director at CIMC, where he sourced, evaluated and managed investments across asset classes and regions for nine years. Mr. Saravia is a Chartered Financial Analyst.'
-  },
-  {
-    name: 'Koen Langendoen',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
-    bio:
-      'Koen Langendoen is a Professor of Computer Science at Delft University of Technology, the Netherlands. He holds the Chair on Embedded Software and has rich experience in systems research, in particular, networking protocols and distributed systems.'
-  },
-  {
-    name: 'Rodolfo Gonzalez',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
-    bio:
-      'Rodolfo Gonzalez is a Partner at Foundation Capital. He is fascinated by financial disintermediation, peer-to-peer funding, and bringing transparency and simplicity to the consumer experience. He holds a MBA from MIT Sloan’s School of Management.'
-  },
-  {
-    name: 'Zavain Dar',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
-    bio:
-      'Zavain Dar is a Principal at Lux Capital focusing on smart software, machine intelligence, and decentralized computing. Previously Zavain was an investor at Eric Schmidt’s Innovation Endeavors. He studied at Stanford and is currently a Lecturer at Stanford where he has taught quarter long seminars in cryptocurrencies, AI, and venture capital.'
+      'JP Singh received his PhD from from Stanford University and is a leading authority on scalable infrastructure and applications. He is the director of the Princeton CTO Program, serves on the board of 8x8, Inc., and has been a consultant to Intel, Microsoft, and the U.S. Government.'
   }
 ]
-export const founders = [
+
+export const advisors = [
   {
-    blockstackId: 'muneeb',
-    name: 'Muneeb Ali',
-    avatar: 'https://s3.amazonaws.com/kd4/muneeb',
+    blockstackId: 'mfreed',
+    name: 'Michael Freedman',
+    title: 'Technical Advisor',
     bio:
-      'Muneeb Ali is a distributed systems PhD from Princeton and gives guest lectures on cloud computing there. He went through Y Combinator and was awarded a J. William Fulbright Fellowship.'
+      'Michael Freedman is a distributed systems Professor at Princeton. He was awarded the Presidential Early Career Award (PECASE), the Sloan Fellowship, and his research has led to commercial products and deployed systems reaching millions of users daily.'
   }
 ]
 
@@ -65,154 +34,132 @@ export const teamMembers = [
   {
     blockstackId: 'judecn',
     name: 'Jude Nelson',
-    avatar: 'https://s3.amazonaws.com/kd4/judecn',
     bio:
       'Jude Nelson earned his PhD in computer science at Princeton and worked as a core member of PlanetLab, which received the ACM Test of Time Award for enabling planetary scale experimentation and deployment. His research covered wide-area storage systems and CDNs. 10+ years of Vim usage.'
   },
   {
     blockstackId: 'blankstein',
     name: 'Aaron Blankstein',
-    avatar: 'https://blockstack.org/images/avatars/aaron-blankstein.jpg',
     bio:
       'Aaron joined the Blockstack engineering team from an academic background, finishing his PhD in 2017. His doctoral research spanned a range of topics, mostly focusing on web application performance and security. He’s worked in information flow tracking systems, caching algorithms, compilers, and applied cryptography. 10+ years of Emacs usage.'
   },
   {
     blockstackId: 'larry',
     name: 'Larry Salibra',
-    avatar: 'https://s3.amazonaws.com/kd4/larry',
     bio:
       'Larry Salibra co-founded Pay4Bugs, a crowdsourced security analysis and software testing platform, and is a founding member of the Bitcoin Association of Hong Kong. His work exposed private data leakage in Chinese Tinder clone Tantan, and how Apple censors apps in China.'
   },
   {
-    blockstackId: 'ginaabrams',
+    blockstackId: 'abramsgina',
     name: 'Gina Abrams',
-    avatar: 'https://blockstack.org/images/avatars/gina-abrams.jpg',
     bio:
       'Gina Abrams studied Psychology and Social Sciences at Binghamton University. Previously she spent two years in agency technical recruiting, during which time she assisted numerous NYC start-ups in mission critical DevOps, SRE and Security hires.'
   },
   {
     blockstackId: 'pstanley',
     name: 'Patrick Stanley',
-    avatar: 'https://blockstack.org/images/avatars/patrick-stanley.jpg',
     bio:
       'Patrick Stanley is the head of growth at Blockstack. He studied Economics and Psychology at Johns Hopkins, and was the first employee at data-driven lending startup Earnest which sold to Navient for $155m in 2017. In a growth role there, he helped it scale from from 1 to 200 employees and go from $0 to $2B in originations within 3 years.'
   },
   {
     blockstackId: 'yukan',
     name: 'Ken Liao',
-    avatar: 'https://blockstack.org/images/avatars/ken-liao.jpg',
     bio:
       'Ken Liao studied Computer Engineering at the University of Waterloo and co-founded the fashion brand marketplace, PR-A-PO. He designed and built mobile apps which were featured as Best New App on the Apple App Store.'
   },
   {
     blockstackId: 'sebastiandunkel',
     name: 'Sebastian Dunkel',
-    avatar: 'https://blockstack.org/images/avatars/sebastian-dunkel.jpg',
     bio:
       'Sebastian received his MA in Linguistics and Communications from the University of Heidelberg, Germany, and the University of Brighton, UK. Before joining the Blockstack team, he worked in localization and product support for Apple, Vimeo, and Master & Dynamic.'
   },
   {
     blockstackId: 'shannon.id.blockstack',
     name: 'Shannon Voight',
-    avatar: 'https://blockstack.org/images/avatars/shannon-voight.jpg',
     bio:
       "Shannon drives event and marketing programs focusing on Blockstack's growth and expansion. Prior to joining Blockstack in 2017, Shannon spent over 11 years honing her skills as event and program producer, talent manager and brand strategist at Condé Nast and Time Inc. "
   },
   {
     blockstackId: 'xan',
     name: 'Xan Ditkoff',
-    avatar: 'https://blockstack.org/images/avatars/xan-ditkoff.jpg',
     bio:
       'Xan came to Blockstack with backgrounds in private equity, investment banking, and the military. He is passionate about driving the creation, growth, and expansion of application ecosystems through his growth role and with the Signature Fund.'
   },
   {
     blockstackId: 'jesse',
     name: 'Jesse Soslow',
-    avatar: 'https://blockstack.org/images/avatars/jesse-soslow.jpg',
     bio:
       'Jesse is Blockstack’s head of legal and finance. Prior to Blockstack, he led operations at Everwise, a Sequoia-backed SaaS startup, and worked as a technology transactions attorney for a large, international law firm.  Jesse received his J.D. and B.A., in math, both from the University of Pennsylvania.'
   },
   {
     blockstackId: 'mitchell',
     name: 'Mitchell Cuevas',
-    avatar: 'https://blockstack.org/images/avatars/mitchell-cuevas.jpg',
     bio:
       'Mitchell is Blockstack’s Marketing Partner. He previously led Marketing at UP Global (Startup Weekend, Startup Digest, Startup Week) before joining the Product team at Techstars. He curates the Martech Digest, advises at Cove Group, and runs ScreenChecker.'
   },
   {
     blockstackId: 'jeffdomke',
     name: 'Jeff Domke',
-    avatar: 'https://blockstack.org/images/avatars/jeff-domke.jpg',
     bio:
       'Jeff leads product at Blockstack. Previously he was CPO and the first hire at CompStak (real estate analytics) where he helped grow the user base 125x, hire 50 team members, and raise 22M in funding. He also previously cofounded Signal (CX analytics) and Capsule.me (consumer messaging).'
   },
   {
     blockstackId: 'hankstoever',
     name: 'Hank Stoever',
-    avatar: 'https://blockstack.org/images/avatars/hank-stoever.jpg',
     bio:
       'Hank is an engineer at Blockstack, working on developer tools and consumer-facing products. He previously worked as an engineer for a variety of startups, including as lead developer for Startup Digest.'
   },
   {
     blockstackId: 'thaonguyenle',
     name: 'Thao-Nguyen Le',
-    avatar: 'https://blockstack.org/images/avatars/thao-nguyen-le.jpg',
     bio:
       'Thao-Nguyen is People and Operations partner at Blockstack, assisting the co-founders and team with organizational prioritization, project management, team collaboration and internal communication. Prior to joining, she led operations in mission driven organizations such as Farmshelf (indoor hydro agri-tech), AntiGravity Fitness and Vietnam Heritage Center. '
   },
   {
     blockstackId: 'ginxh.id.blockstack',
     name: 'Virginia Hickox',
-    avatar: 'https://blockstack.org/images/avatars/virginia-hickox.jpg',
     bio:
       'Virginia is a Blockstack Engineering Partner, focusing on DevOps, developing microservices and infrastructure for the decentralized web. She has a degree in Electrical Engineering from RPI, and previously worked as an Engineer in Operations for open market access for electrical power. She is interested in information privacy and democracy in the digital world.'
   },
   {
     blockstackId: 'liz.id.blockstack',
     name: 'Elizabeth Thys',
-    avatar: 'https://blockstack.org/images/avatars/elizabeth-thys.jpg',
     bio:
       'Liz is a Blockstack Operations Partner. Prior to Blockstack, she co-founded limeSHIFT, a creative agency that aligns people, place and purpose, which emerged from a collaboration between MIT Sloan School of Management and MIT Department of Urban Studies and Planning. She received a BS in Mechanical Engineering from Tufts University and an MBA from MIT Sloan School of Management. '
   },
   {
     blockstackId: 'shrey',
     name: 'Shreyas Thiagaraj',
-    avatar: 'https://blockstack.org/images/avatars/shreyas-thiagaraj.jpg',
     bio:
       'Shreyas is a Blockstack Engineering Partner, working on developer tools and mobile efforts. He comes from a background in product development for various social apps, and before that built developer tools for mobile apps as an engineer at Microsoft. When not at work, you can find him on the hunt for the world’s best hot chocolate.'
   },
   {
     blockstackId: 'mmanthony.id.blockstack',
     name: 'Mary Anthony',
-    avatar: 'https://blockstack.org/images/avatars/mary-anthony.jpg',
     bio:
       'Mary joined Blockstack as a Documentation Engineer. She has co-founded a CRO, managed teams, developed documentation deployment infrastructure, and designed user interfaces. Her experience includes developing content for open source, clinical research, container technologies, security software, business intelligence software, remote source control software, and system administration. She spends her non-work time studying figurative art, hiking, and trail running.'
   },
   {
     blockstackId: 'jwiley.id.blockstack',
     name: 'Jesse Wiley',
-    avatar: 'https://blockstack.org/images/avatars/jesse-wiley.png',
     bio:
       "Jesse is an Engineering Partner, focusing on DevOps, automating micro-services and infrastructure-as-code for the decentralized web. After spending too much time at large companies, he's bringing the automation and innovation to Blockstack, working to make everyone's job just a little bit easier."
   },
   {
     blockstackId: 'aulneau',
     name: 'Thomas Osmonson',
-    avatar: 'https://blockstack.org/images/avatars/thomas-osmonson.jpg',
     bio:
       'Thomas Osmonson is a product engineer working at the boundary of design and development. He likes wearing many hats ranging from visual design to building performant web/native applications. Previously as an independent, he has helped agencies, startups, and founders build and design useful, beautiful products. He has an affinity for the wilderness, street photography, and traveling.'
   },
   {
     blockstackId: 'markmhendrickson',
     name: 'Mark Hendrickson',
-    avatar: 'https://blockstack.org/images/avatars/mark-hendrickson.jpg',
     bio:
       'Mark is a product manager from San Francisco with over a decade designing, developing and marketing Web and mobile applications for startups. He now lives in Barcelona in self-imposed exile having launched a number of products in Silicon Valley such as Plancast, KITE, Lift and CrunchBase.'
   },
   {
     blockstackId: 'fredericklee',
     name: 'Frederick Lee',
-    avatar: 'https://blockstack.org/images/avatars/frederick-lee.jpg',
     bio: (
       <>
         Freddie is Associate General Counsel at Blockstack. Prior to Blockstack,
@@ -224,24 +171,5 @@ export const teamMembers = [
         <em>Law Review</em>.
       </>
     )
-  }
-]
-
-export const advisors = [
-  {
-    blockstackId: 'mfreed',
-    name: 'Michael Freedman',
-    title: 'Technical Advisor',
-    avatar: 'https://blockstack.org/images/avatars/mike-freedman.jpg',
-    bio:
-      'Michael Freedman is a distributed systems Professor at Princeton. He was awarded the Presidential Early Career Award (PECASE), the Sloan Fellowship, and his research has led to commercial products and deployed systems reaching millions of users daily.'
-  },
-  {
-    blockstackId: 'jpsingh',
-    name: 'JP Singh',
-    title: 'Technical Advisor',
-    avatar: 'https://blockstack.org/images/avatars/jp-singh.jpg',
-    bio:
-      'JP Singh received his PhD from from Stanford University and is a leading authority on scalable infrastructure and applications. He is the director of the Princeton CTO Program, serves on the board of 8x8, Inc., and has been a consultant to Intel, Microsoft, and the U.S. Government.'
   }
 ]
