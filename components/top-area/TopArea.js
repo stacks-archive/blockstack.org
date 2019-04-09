12f3b83b20b89b62741c92c16f7ebd2ffa36a126
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-04-09 17:57:17 -0500

    resolves: #758, #757, #755

--- components/top-area/TopArea.js
+++ components/top-area/TopArea.js
@@ -49,93 +49,106 @@ const TopArea = (props) => {
           <div>
             <div className="grid-flex v-spaced break-xlg">
               <div className="col-4">
-                  <a className="g-card code-card dark align-left link" href="https://docs.blockstack.org/browser/hello-blockstack.html" target="_blank">
+                <a
+                  className="g-card code-card dark align-left link"
+                  href="https://docs.blockstack.org/browser/hello-blockstack.html"
+                  target="_blank"
+                >
+                  <div className="p-2">
+                    <CodeBlock
+                      className="sm minimal dark"
+                      language="javascript"
+                    >
+                      {UserCode}
+                    </CodeBlock>
+                  </div>
+                  <Card className="card">
                     <div className="p-2">
-                      <CodeBlock
-                        className="sm minimal dark"
-                        language="javascript"
-                      >
-                        {UserCode}
-                      </CodeBlock>
-                    </div>
-                    <Card className="card">
-                      <div className="p-2">
-                        <div className="grid-flex tight-gutter no-break pb-2">
-                          <div className="col grow">
-                            <h4 className="main-color underline-hover">
-                              Hello World
-                            </h4>
-                          </div>
-                          <div className="col no-grow">
-                            <div className="bs-outline-arrow">
-                              <Arrow />
-                            </div>
+                      <div className="grid-flex tight-gutter no-break pb-2">
+                        <div className="col grow">
+                          <h4 className="main-color underline-hover">
+                            Hello World
+                          </h4>
+                        </div>
+                        <div className="col no-grow">
+                          <div className="bs-outline-arrow">
+                            <Arrow />
                           </div>
                         </div>
-                        <p className="sm">
-                          Build a single-page JavaScript application that runs
-                          completely client-side without any servers.
-                        </p>
                       </div>
-                    </Card>
-                  </a>
+                      <p className="sm">
+                        Build a single-page JavaScript application that runs
+                        completely client-side without any servers.
+                      </p>
+                    </div>
+                  </Card>
+                </a>
               </div>
               <div className="col-4">
-                  <a className="g-card code-card dark align-left link" href="https://docs.blockstack.org/develop/zero_to_dapp_1.html" target="_blank">
+                <a
+                  className="g-card code-card dark align-left link"
+                  href="https://docs.blockstack.org/develop/zero_to_dapp_1.html"
+                  target="_blank"
+                >
+                  <div className="p-2">
+                    <CodeBlock className="sm minimal dark">
+                      {AuthCode}
+                    </CodeBlock>
+                  </div>
+                  <Card className="card">
                     <div className="p-2">
-                      <CodeBlock className="sm minimal dark">
-                        {AuthCode}
-                      </CodeBlock>
-                    </div>
-                    <Card className="card">
-                      <div className="p-2">
-                        <div className="grid-flex tight-gutter no-break pb-2">
-                          <div className="col grow">
-                            <h4 className="main-color underline-hover">
-                              Zero-to-DApp
-                            </h4>
-                          </div>
-                          <div className="col no-grow">
-                            <div className="bs-outline-arrow">
-                              <Arrow />
-                            </div>
+                      <div className="grid-flex tight-gutter no-break pb-2">
+                        <div className="col grow">
+                          <h4 className="main-color underline-hover">
+                            Zero-to-DApp
+                          </h4>
+                        </div>
+                        <div className="col no-grow">
+                          <div className="bs-outline-arrow">
+                            <Arrow />
                           </div>
                         </div>
-                        <p className="sm">
-                        Build a DApp to learn about the Blockstack platform and how to join App Mining.
-                        </p>
                       </div>
-                    </Card>
-                  </a>
+                      <p className="sm">
+                        Build a DApp to learn about the Blockstack platform and
+                        how to join App Mining.
+                      </p>
+                    </div>
+                  </Card>
+                </a>
               </div>
               <div className="col-4">
-                  <a className="g-card code-card dark align-left link" href="https://docs.blockstack.org/browser/blockstack_storage.html" target="_blank">
+                <a
+                  className="g-card code-card dark align-left link"
+                  href="https://docs.blockstack.org/browser/blockstack_storage.html"
+                  target="_blank"
+                >
+                  <div className="p-2">
+                    <CodeBlock className="sm minimal dark">
+                      {StorageCode}
+                    </CodeBlock>
+                  </div>
+                  <Card className="card">
                     <div className="p-2">
-                      <CodeBlock className="sm minimal dark">
-                        {StorageCode}
-                      </CodeBlock>
-                    </div>
-                    <Card className="card">
-                      <div className="p-2">
-                        <div className="grid-flex tight-gutter no-break pb-2">
-                          <div className="col grow">
-                            <h4 className="main-color underline-hover">
-                              Storage
-                            </h4>
-                          </div>
-                          <div className="col no-grow">
-                            <div className="bs-outline-arrow">
-                              <Arrow />
-                            </div>
+                      <div className="grid-flex tight-gutter no-break pb-2">
+                        <div className="col grow">
+                          <h4 className="main-color underline-hover">
+                            Storage
+                          </h4>
+                        </div>
+                        <div className="col no-grow">
+                          <div className="bs-outline-arrow">
+                            <Arrow />
                           </div>
                         </div>
-                        <p className="sm">
-                          Build a decentralized blogging app using multi-player
-                          Gaia storage.
-                        </p>
                       </div>
-                    </Card>
-                  </a>
+                      <p className="sm">
+                        Build a decentralized blogging app using multi-player
+                        Gaia storage.
+                      </p>
+                    </div>
+                  </Card>
+                </a>
               </div>
             </div>
           </div>
@@ -144,18 +157,23 @@ const TopArea = (props) => {
           <nav className="line-nav pb-4">
             <ul className="pb-3">
               <li>
-                <a href={props.links.tutorials} target="blank">
+                <a href={props.links.tutorials} target="_blank">
                   All tutorials
                 </a>
               </li>
               <li>
-                <a href={props.links.documentation} target="blank">
+                <a href={props.links.documentation} target="_blank">
                   Documentation
                 </a>
               </li>
               <li>
-                <a href={props.links.github} target="blank">
+                <a href={props.links.github} target="_blank">
                   GitHub
+                </a>
+              </li>
+              <li>
+                <a href={props.links.roadmap} target="_blank">
+                  Roadmap
                 </a>
               </li>
             </ul>
--- pages/home/index.js
+++ pages/home/index.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import { bslinks } from '@common'
 import TopArea from '@components/top-area'
 import AppsGrid from '@components/apps-grid'
-import WhyBS from '@components/why-bs'
 import OurTechnology from '@components/our-technology'
 import OurCommunity from '@components/our-community'
-import FundApp from '@components/fund-app'
-import Mission from '@components/mission'
+import { News } from '@components/news'
+import fetch from 'cross-fetch'
 
 const meta = {
   path: '/',
@@ -14,24 +13,33 @@ const meta = {
   template: 'NONE'
 }
 
-class HomePage extends React.PureComponent {
+class HomePage extends React.Component {
   static async getInitialProps(ctx) {
+    const feedData = await fetch(
+      'https://blog.blockstack.org/wp-json/wp/v2/posts'
+    )
+    const usersData = await fetch(
+      'https://blog.blockstack.org/wp-json/wp/v2/users?per_page=30'
+    )
+    const feed = await feedData.json()
+    const users = await usersData.json()
+
     return {
-      meta
+      meta,
+      feed,
+      users
     }
   }
   render() {
-    const { data } = this.props
+    const { data, feed, users } = this.props
 
     return (
       <div>
         <TopArea links={bslinks} />
         <AppsGrid links={bslinks} />
-        <WhyBS />
         <OurTechnology />
         <OurCommunity links={bslinks} />
-        <FundApp links={bslinks} />
-        <Mission />
+        <News items={feed} users={users} />
       </div>
     )
   }
