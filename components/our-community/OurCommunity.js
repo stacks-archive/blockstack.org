6be8606178129e1713c3d80aca35b4bc0769f3dc
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-04-10 08:24:08 -0500

    remove berlin box #762

--- components/our-community/OurCommunity.js
+++ components/our-community/OurCommunity.js
@@ -90,40 +90,6 @@ class OurCommunity extends Component {
                     </div>
                   )
                 })}
-                <div className="col-12">
-                  <Card className="card" href="/berlin2018/">
-                    <div className="grid-flex tight-gutter middle break-xlg">
-                      <div className="col-4 logo-col">
-                        <Image
-                          noBlur
-                          src="images/bs-berlin.png"
-                          alt="Blockstack Berlin"
-                          className="mx-auto"
-                        />
-                      </div>
-                      <div className="col-8">
-                        <div className="grid-flex tight-gutter middle">
-                          <div className="col grow">
-                            <div className="p-2 max-text-width">
-                              <p className="sm">
-                                Fri, March 2, 2018: Experts on privacy,
-                                blockchain, information theory, and the
-                                decentralized community.
-                              </p>
-                            </div>
-                          </div>
-                          <div className="col no-grow">
-                            <div className="p-2 pr-gutter">
-                              <span className="bs-outline-arrow inline">
-                                <Arrow />
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Card>
-                </div>
               </div>
             </div>
           </div>
