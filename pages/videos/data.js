f498a45058df980c3d0bc10b2e5468f98bc125be
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-04-10 17:30:30 -0500

    update video content

--- pages/videos/data.js
+++ pages/videos/data.js
@@ -1,13 +1,12 @@
 export const videoData = [
   {
-    title: "A New Internet Where You're In Control",
-    event: 'TheNextWeb 2017',
-    speaker: 'Ryan Shea',
-    location: 'New York, NY',
-    date: 'December 12, 2017',
-    youtubeURL: 'https://www.youtube.com/watch?v=iBIcQaBoslM',
-    urlSlug: 'thenextweb-2017-a-new-internet-where-youre-in-control',
-    image: 'images/videos/a_new_internet.jpg'
+    title: 'What will replace Facebook?',
+    event: 'The Economist',
+    date: 'February 4, 2019',
+    youtubeURL:
+      'https://www.youtube.com/watch?v=MvWZ8TrM41w&feature=youtu.be&t=92',
+    urlSlug: 'what-will-replace-facebook',
+    image: 'images/videos/economist-video-thumb.png'
   },
   {
     title: 'The Future of the Internet is Decentralized',
@@ -19,26 +18,6 @@ export const videoData = [
     urlSlug: 'the-conference-the-future-of-the-internet-is-decentralized',
     image: 'images/videos/the_future_is_decentralized.jpg'
   },
-  // {
-  //   title: 'A New Blockstack Internet',
-  //   event: 'Blockstack Summit 2017',
-  //   speaker: 'Ryan Shea',
-  //   location: 'Mountain View, CA',
-  //   date: 'July 27, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=YzlyEuRfXxo',
-  //   urlSlug: 'blockstack-summit-2017-a-new-blockstack-internet',
-  //   image: 'images/videos/a_new_blockstack_internet.jpg'
-  // },
-  // {
-  //   title: 'The Next Wave of Computing',
-  //   event: 'Blockstack Summit 2017',
-  //   speaker: 'Muneeb Ali',
-  //   location: 'Mountain View, CA',
-  //   date: 'July 27, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=IfONVXL6tnk',
-  //   urlSlug: 'blockstack-summit-2017-the-next-wave-of-computing',
-  //   image: 'images/videos/the_next_wave_of_computing.jpg'
-  // },
   {
     title: 'A Conversation with Naval Ravikant and Ryan Shea',
     event: 'Blockstack Summit 2017',
@@ -49,103 +28,5 @@ export const videoData = [
     urlSlug:
       'blockstack-summit-2017-a-conversation-with-naval-ravikant-and-ryan-shea',
     image: 'images/videos/convo_naval_ryan.jpg'
-  },
-  // {
-  //   title:
-  //     '"A New Decentralized App Ecosystem on Blockstack" | Blockstack Summit 2017',
-  //   event: 'Blockstack Summit 2017',
-  //   speaker: 'Patrick Stanley',
-  //   location: 'Mountain View, CA',
-  //   date: 'July 27, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=m3pwRj68zQ0',
-  //   urlSlug:
-  //     'blockstack-summit-2017-a-new-decentralized-app-ecosystem-on-blockstack-blockstack-summit-2017',
-  //   image: 'images/videos/summit_2017.jpg'
-  // },
-  // {
-  //   title:
-  //     "Q&A w/ Blockstack's Muneeb Ali & Ryan Shea, Moderated by Alice Lloyd George",
-  //   event: 'Blockstack Summit 2017',
-  //   speaker: 'Muneeb Ali, Ryan Shea, Alice Lloyd George',
-  //   location: 'Mountain View, CA',
-  //   date: 'July 27, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=JY5EMjHe4-Y',
-  //   urlSlug:
-  //     'blockstack-summit-2017-qa-w-blockstacks-muneeb-ali-ryan-shea-moderated-by-alice-lloyd-george',
-  //   image: 'images/videos/q_a_ryan_muneeb.jpg'
-  // },
-  // {
-  //   title:
-  //     'Blockstack: A New Internet That Brings Privacy & Property Rights to Cyberspace',
-  //   event: 'Reason TV',
-  //   speaker: 'Ryan Shea + Muneeb Ali',
-  //   location: 'New York, NY',
-  //   date: 'June 22, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=Z4bMFKBRg_k',
-  //   urlSlug:
-  //     'reason-tv-blockstack-a-new-internet-that-brings-privacy-property-rights-to-cyberspace',
-  //   image: 'https://i.ytimg.com/vi/Z4bMFKBRg_k/maxresdefault.jpg'
-  // },
-  // {
-  //   title: 'Building a New Decentralized Internet',
-  //   event: 'Consensus 2017',
-  //   speaker: 'Muneeb Ali + Ryan Shea',
-  //   location: 'New York, NY',
-  //   date: 'May 23, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=VZGyS5JobkY',
-  //   urlSlug: 'consensus-2017-building-a-new-decentralized-internet',
-  //   image: 'https://i.ytimg.com/vi/VZGyS5JobkY/maxresdefault.jpg'
-  // },
-  // {
-  //   title: 'Building a Foundation for Decentralized Identity',
-  //   event: 'Consensus 2017',
-  //   speaker: 'Ryan Shea',
-  //   location: 'New York, NY',
-  //   date: 'May 22, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=tsPXWixkrFk',
-  //   urlSlug: 'consensus-2017-building-a-foundation-for-decentralized-identity',
-  //   image: 'https://i.ytimg.com/vi/tsPXWixkrFk/maxresdefault.jpg'
-  // },
-  // {
-  //   title: 'Blockchain Powered Decentralized Applications with Blockstack',
-  //   event: 'NYCNode',
-  //   speaker: 'Larry Salibra',
-  //   location: 'New York, NY',
-  //   date: 'May 17, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=uG06irysjHg',
-  //   urlSlug:
-  //     'nycnode-blockchain-powered-decentralized-applications-with-blockstack',
-  //   image: 'https://i.ytimg.com/vi/uG06irysjHg/maxresdefault.jpg'
-  // },
-  // {
-  //   title: 'The Decentralized Internet with Blockstack',
-  //   event: 'The Lunduke Hour',
-  //   speaker: 'Ryan Shea',
-  //   location: 'New York, NY',
-  //   date: 'April 17, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=i1fThdPbAEA',
-  //   urlSlug: 'the-lunduke-hour',
-  //   image: 'https://i.ytimg.com/vi/i1fThdPbAEA/maxresdefault.jpg'
-  // },
-  // {
-  //   title: 'How to Create a Blockchain Identity with Bitcoin (and Ethereum)',
-  //   event: 'Bitcoin Barcelona',
-  //   speaker: 'Ryan Shea',
-  //   location: 'Barcelona, Spain',
-  //   date: 'February 9, 2017',
-  //   youtubeURL: 'https://www.youtube.com/watch?v=dCNC2SjqYHc',
-  //   urlSlug: 'decentralized-apps-and-sovereign-identity',
-  //   image: 'https://i.ytimg.com/vi/dCNC2SjqYHc/maxresdefault.jpg'
-  // },
-  {
-    title: 'Welcome to the New Internet',
-    event: 'TEDxNewYork',
-    speaker: 'Muneeb Ali',
-    location: 'New York, NY',
-    date: 'September 10, 2016',
-    youtubeURL: 'https://www.youtube.com/watch?v=qtOIh93Hvuw',
-    speakerDeckID: '15d4861b7c1842bfbd7add80c99e1cf6',
-    urlSlug: 'welcome-to-the-new-internet',
-    image: 'https://i.ytimg.com/vi/qtOIh93Hvuw/maxresdefault.jpg'
   }
 ]
