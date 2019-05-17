77685948ca652424fc16e5c782cd40fae794b477
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-05-17 12:46:32 -0500

    try-blockstack route

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const routes = async () => {
     '/': { page: '/' },
     '/about': { page: '/about' },
     '/technology': { page: '/technology' },
+    '/try-blockstack': { page: '/try-blockstack' },
     '/404': { page: '/_error' },
     '/pbc': { page: '/about' },
     // '/blog': { page: '/blog' },
